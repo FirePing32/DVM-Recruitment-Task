e603991d45ecdd51d2737bdd4022be6acb00caab
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-05-28 17:57:40 +0530

    setup ssl, ton of changes

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -37,3 +37,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
